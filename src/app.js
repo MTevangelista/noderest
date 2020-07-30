2d612cacb6fa8eb4c6a6a6f51f8b656d0b957550
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-07-30 16:57:46 -0300

    MongoDB setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
+require("dotenv").config()
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const app = express();
 const router = express.Router();
 
 // Settings
     // Body Parser
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: false }))
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({ extended: false }))
+    // Mongo
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }).then(() => {
+            console.log('MongoDB connected');
+        }).catch((err) => {
+            console.log(`Connection error: ${err}`);
+        })
 //  Routes
 const indexRoute = require('./routes/index')
 
